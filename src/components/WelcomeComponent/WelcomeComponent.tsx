b25a1050e5ede9718d483af8002830f14474d128
austenoutoften <austin.bev@hotmail.com>
2023-09-17 15:47:50 +0800

    style: animations

--- src/components/WelcomeComponent/WelcomeComponent.tsx
+++ src/components/WelcomeComponent/WelcomeComponent.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'; 
 import { Link } from 'react-router-dom'; 
+import { motion } from 'framer-motion';
 
 import './WelcomeComponent.css'
 
@@ -17,6 +18,12 @@ function WelcomeComponent(props : any) {
   
   
   return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 20 }}
+      transition={{ duration: 0.5 }}
+    >
   <div>
     <a onClick={props.toggleTheme}>Toggle Dark Mode</a>
     <div className='welcome-div'>
@@ -39,6 +46,7 @@ function WelcomeComponent(props : any) {
       </Link>
     </div>
   </div>
+  </motion.div>
   );
 }
 
